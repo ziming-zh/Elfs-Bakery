9f82e7756a885986b7ccbf37bc927d363d823a3f
Liu Qijia <smile.pony@sjtu.edu.cn>
2022-06-28 21:50:25 +0800

    feat: Player

--- src/Model.elm
+++ src/Model.elm
@@ -1,66 +1,66 @@
 module Model exposing (..)
-import LevelSeq
 
-import Random
-import Levels exposing (Level)
-import Task
 import Array exposing (Array)
-import Message exposing (Msg,Page(..),Block,MoveDirection(..))
-import Valve exposing (Valve)
-import LevelSeq exposing (LevelSeq)
-import Levels exposing (EncodeLevel)
-import Message exposing (Msg(..))
 import Color exposing (Color)
+import LevelSeq exposing (LevelSeq)
+import Levels exposing (EncodeLevel, Level)
+import Message exposing (MoveDirection(..), Msg(..), Page(..), Paint, Paints, Pos)
+import Player
+import Random
+import Valve exposing (Valve)
+import Wall exposing (Wall)
 
 
 type alias Model =
     Mapset
-    { win : Bool
-    , elapse : Float
-    , levels : LevelSeq
-    , currentLevel : EncodeLevel
-    , level_index : Int
-    , valves_move : Int
-    , history : List GameState
-    , currentPage : Page
-    , lastMoveDirection : MoveDirection
---  , stringlevel : StringLevel
-    , randomindex : Int
-    , windowsize : ( Float, Float )
-    }
+        { win : Bool
+        , move_timer : Float
+        , levels : LevelSeq
+        , currentLevel : EncodeLevel
+        , level_index : Int
+        , valves_move : Int
+        , history : List GameState
+        , currentPage : Page
 
-type alias Flags = { levels : Maybe String}
-    
+        --, lastMoveDirection : MoveDirection  --merge the direction of the player into Type Player
+        --  , stringlevel : StringLevel
+        , randomindex : Int
+        , windowsize : ( Float, Float )
+        }
+
+
+type alias Flags =
+    { levels : Maybe String }
+
+
 type alias Mapset a =
     { a
-        | player : Block
-        , wall : List Block
+        | player : Player.Model
+        , wall : Wall
         , valves : List Valve
-        , paints : List(List Block, Color)
-        , dots : List Block
+        , paints : List Paints
+        , dots : List Pos --what is dots
         , mapSize : ( Int, Int )
     }
 
-type alias GameState = 
+
+type alias GameState =
     { gamestate : CurState
-    , laststate : CurState {-undo to the previous scenario-}
+    , laststate : CurState
+
+    {- undo to the previous scenario -}
     }
+
 
 type alias CurState =
-    { player : Block
+    { player : Player.Model
     , valves : List Valve
-    , paints : List (List Block, Color)
+    , paints : List Paints
     }
-
-
-
-
-
-
 
 
 {-| load level, new or existing
-set level_index in each case
+set level\_index in each case
 -}
 checkAndLoadGameWithLevel : EncodeLevel -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 checkAndLoadGameWithLevel encodedLevel ( model, cmd ) =
@@ -68,10 +68,11 @@ checkAndLoadGameWithLevel encodedLevel ( model, cmd ) =
         isExistingLevel =
             LevelSeq.isDuplicate encodedLevel model.levels
     in
-        if isExistingLevel then
-            loadGameWithExistingLevel encodedLevel ( model, cmd )
-        else
-            loadGameWithNewLevel encodedLevel ( model, cmd )
+    if isExistingLevel then
+        loadGameWithExistingLevel encodedLevel ( model, cmd )
+
+    else
+        loadGameWithNewLevel encodedLevel ( model, cmd )
 
 
 loadGameWithExistingLevel : EncodeLevel -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
@@ -83,11 +84,11 @@ loadGameWithExistingLevel level ( model, cmd ) =
         levelIndex =
             LevelSeq.getIndexOf level model.levels
     in
-        ( { modelWithGame
-            | level_index = levelIndex
-          }
-        , cmd
-        )
+    ( { modelWithGame
+        | level_index = levelIndex
+      }
+    , cmd
+    )
 
 
 loadGameWithNewLevel : EncodeLevel -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
@@ -99,79 +100,80 @@ loadGameWithNewLevel level ( model, cmd ) =
         newLevels =
             LevelSeq.prependLevel level model.levels
     in
-        ( { modelWithGame
-            | levels = newLevels
-            , level_index = 0
-          }
-        , Cmd.batch
-            [ cmd
-            -- , Storage.storeLevels newLevels
-            ]
-        )
+    ( { modelWithGame
+        | levels = newLevels
+        , level_index = 0
+      }
+    , Cmd.batch
+        [ cmd
+
+        -- , Storage.storeLevels newLevels
+        ]
+    )
 
 
 
+{- Loading Levels to activate the game
 
-
-
-{-  Loading Levels to activate the game
-
-loadGameWithLevel : Level -> Model -> Model
-loadGameWithLevel encodedLevel model =
-    let
-        game =
-            decodeLevel encodedLevel
-    in
-        { player = game.player
-        , wall = game.walls
-        , paints = game.paints
-        , valves = game.valves
-        , dots = game.dots
-        , mapSize = game.gameSize
-        , win = False
-        , lastMoveDirection = Up
-        , elapse = 0.0
-        , levels = model.levels
-        , currentLevel = encodedLevel
-        , level_index = model.level_index 
-        , valves_move = 0
-        , history = []
-        , currentPage = GamePage
-        , windowsize = model.windowsize
-        , randomindex = model.randomindex
-        }
+   loadGameWithLevel : Level -> Model -> Model
+   loadGameWithLevel encodedLevel model =
+       let
+           game =
+               decodeLevel encodedLevel
+       in
+           { player = game.player
+           , wall = game.walls
+           , paints = game.paints
+           , valves = game.valves
+           , dots = game.dots
+           , mapSize = game.gameSize
+           , win = False
+           , lastMoveDirection = Up
+           , elapse = 0.0
+           , levels = model.levels
+           , currentLevel = encodedLevel
+           , level_index = model.level_index
+           , valves_move = 0
+           , history = []
+           , currentPage = GamePage
+           , windowsize = model.windowsize
+           , randomindex = model.randomindex
+           }
 
 -}
+
 
 loadGameWithLevel : EncodeLevel -> Model -> Model
-loadGameWithLevel encodedLevel model = model
+loadGameWithLevel encodedLevel model =
+    model
+
+
 {-| only when init the app
 -}
-
-initModel :  ( Model, Cmd Msg )
-initModel  =
+initModel : ( Model, Cmd Msg )
+initModel =
     let
-        levels = LevelSeq.getInitialLevels
+        levels =
+            LevelSeq.getInitialLevels
     in
-        ( { player = Block 0 0
-          , wall = []
-          , paints = []
-          , valves = []
-          , dots = []
-          , mapSize = ( 0, 0 )
-          , win = False
-          , lastMoveDirection = Down
-          , levels = levels -- important here
-          , elapse = 0.0
-          , currentLevel = ""
-          , level_index = 0
-          , valves_move = 0
-          , history = []
-          , currentPage = HomePage
-          , windowsize = (800,800)
-          , randomindex = 0
-          }
-        , Cmd.batch
-            [Random.generate RandomLevel (Random.int 0 39)
-            ]
-        )
+    ( { player = Player.init
+      , wall = []
+      , paints = []
+      , valves = []
+      , dots = []
+      , mapSize = ( 0, 0 )
+      , win = False
+      , levels = levels -- important here
+      , elapse = 0.0
+      , currentLevel = ""
+      , level_index = 0
+      , valves_move = 0
+      , history = []
+      , currentPage = HomePage
+      , windowsize = ( 800, 800 )
+      , randomindex = 0
+      }
+    , Cmd.batch
+        [ Random.generate RandomLevel (Random.int 0 39)
+        ]
+    )
---
+++ src/Wall.elm
@@ -0,0 +1,54 @@
+module Wall exposing (Wall, isWall)
+
+import Array exposing (fromList, get)
+import Maybe exposing (withDefault)
+import Message exposing (MoveDirection(..), Pos)
+
+
+type alias Wall_row =
+    List (List Bool)
+
+
+type alias Wall_col =
+    List (List Bool)
+
+
+type alias Wall =
+    { col : Wall_col, row : Wall_row }
+
+
+isWall : { pos : Pos, dir : MoveDirection } -> Wall -> Bool
+isWall { pos, dir } wall =
+    let
+        x =
+            pos.x
+
+        y =
+            pos.y
+
+        wcol =
+            wall.col
+
+        wrow =
+            wall.row
+    in
+    case dir of
+        Up ->
+            getWall wrow x y
+        Down ->
+            getWall wrow x (y+1)
+        Left ->
+            getWall wcol x y
+        Right -> 
+            getWall wcol (x+1) y
+        Stop ->
+            True
+            
+getWall: (List (List Bool))-> Int -> Int -> Bool -- a helper function to get wall(x,y)
+getWall wall x y =
+    get y (fromList wall)
+    |> withDefault []
+    |> fromList
+    |> get x
+    |> withDefault True
+--confused whether the order of x and y is correct
