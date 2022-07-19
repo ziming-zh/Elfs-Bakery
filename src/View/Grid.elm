07561adcea76fb54c5fbbc616036483ea9402abd
yangchongye <1035719430@sjtu.edu.cn>
2022-07-20 02:59:56 +0800

    feat: color according to distance adjustment

--- src/View/Grid.elm
+++ src/View/Grid.elm
@@ -11,21 +11,32 @@ import Grid exposing (GridType(..))
 
 renderGrids : Grids -> List Renderable
 renderGrids grids = 
-    List.map (\y -> Array.toList (Array.map (\x-> drawGrid x) y)) (Array.toList grids)
-    |> List.foldl List.append []
+    let
+        listgrid =  Array.toList ( Array.map ( \x -> Array.toList x ) grids )
+        listgrids = List.concat listgrid
+        listdis = List.map  (
+                            \x -> 
+                                case x.distance of 
+                                    Nothing -> 0 
+                                    Just a -> a
+                            ) listgrids
+        mx = Basics.max (List.foldl Basics.max 0 listdis) 1
+    in
+        List.map (\y -> Array.toList (Array.map (\x-> drawGrid (Basics.toFloat mx) x) y)) (Array.toList grids)
+        |> List.foldl List.append []
 
 
-drawGrid : Grid -> Renderable
-drawGrid grid =
+drawGrid : Float -> Grid -> Renderable
+drawGrid mx grid =
     let
         posy = (toFloat grid.pos.x) * setLength
         posx = (toFloat grid.pos.y) * setLength
-        r = 254-(Maybe.withDefault 0 grid.distance) * 1
-        g = 248-(Maybe.withDefault 0 grid.distance) * 4
-        b = 231-(Maybe.withDefault 0 grid.distance) * 1
+        r = (254-(Basics.toFloat (Maybe.withDefault 0 grid.distance))/mx * 20)/255
+        g = (248-(Basics.toFloat (Maybe.withDefault 0 grid.distance))/mx * 80)/255
+        b = (231-(Basics.toFloat(Maybe.withDefault 0 grid.distance))/mx * 20)/255
         color = 
             case grid.gridtype of
-                Vacant -> Color.rgb255 r g b 
+                Vacant -> Color.rgb r g b 
                 Exit -> Color.lightGray
                 Paint paint -> paint.color
     in
